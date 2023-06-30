0ff8242a54ab4c683066f719aa2e6cbe6bce4128
helicop0420 <helicop0420@gmail.com>
2023-06-30 16:58:10 +0000

    feat: add edit faqs

--- resources/views/backendV2/management/faqs/list.blade.php
+++ resources/views/backendV2/management/faqs/list.blade.php
@@ -8,6 +8,87 @@
 @endsection
 
 <style>
+.modal {
+    --bs-modal-width: 40%
+}
+
+.modal.left .modal-dialog {
+    position: fixed;
+    right: 0;
+    margin: auto;
+    width: 1000px;
+    height: 100%;
+    -webkit-transform: translate3d(0%, 0, 0);
+    -ms-transform: translate3d(0%, 0, 0);
+    -o-transform: translate3d(0%, 0, 0);
+    transform: translate3d(0%, 0, 0);
+}
+
+.modal.left .modal-content {
+    height: 100%;
+    overflow-y: auto;
+    color: #fff !important;
+}
+
+/* ----- MODAL STYLE ----- */
+.modal-content {
+    border-radius: 0;
+    border: none;
+    background-color: #151515;
+}
+
+.modal-header {
+    border-bottom-color: #eeeeee;
+    background-color: #fafafa;
+}
+
+.modal-body div .card-selected {
+    padding: 20px;
+    outline: solid;
+    outline-width: 2px;
+    outline-color: #1d1d1d;
+    background-color: #151515 !important;
+}
+
+.modal-body div .card {
+    padding: 20px;
+    outline: solid;
+    outline-width: 2px;
+    outline-color: #1d1d1d;
+}
+
+.modal-body p {
+    color: #7d7d7d;
+}
+
+.modal-body textarea {
+    background-color: #151515;
+}
+
+.modal-body select,
+.modal-body textarea {
+    border-color: #1d1d1d;
+}
+
+.modal-body select {
+    background-color: #151515;
+}
+
+.modal-body .card-footer {
+    border-top-color: #7d7d7d;
+}
+
+
+.table.gy-5 td,
+.table.gy-5 th {
+    padding-top: 5px !important;
+    padding-bottom: 5px !important;
+}
+
+.modal .uname {
+    color: #fff;
+}
+
 .table.gy-5 td,
 .table.gy-5 th {
     padding-top: 5px !important;
@@ -620,8 +701,9 @@ h1.d-flex.text-dark.fw-bold.my-0.fs-1 {
                                 @endif
                             </td>
                             <td style="font-size: 20px;">
-                                <a href="{{ route('backend-management-faq-edit', $faq->id) }}"><svg width="24"
-                                        height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <div class="btn btn-icon btn-color-gray-700 btn-active-color-primary w-40px h-40px p-2"
+                                    data-bs-toggle="modal" data-bs-target="#myModal"><svg width="24" height="24"
+                                        viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path
                                             d="M11 4H4C3.46957 4 2.96086 4.21071 2.58579 4.58579C2.21071 4.96086 2 5.46957 2 6V20C2 20.5304 2.21071 21.0391 2.58579 21.4142C2.96086 21.7893 3.46957 22 4 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V13"
                                             stroke="#BBBBBB" stroke-width="2" stroke-linecap="round"
@@ -630,7 +712,7 @@ h1.d-flex.text-dark.fw-bold.my-0.fs-1 {
                                             d="M18.5 2.50023C18.8978 2.1024 19.4374 1.87891 20 1.87891C20.5626 1.87891 21.1022 2.1024 21.5 2.50023C21.8978 2.89805 22.1213 3.43762 22.1213 4.00023C22.1213 4.56284 21.8978 5.1024 21.5 5.50023L12 15.0002L8 16.0002L9 12.0002L18.5 2.50023Z"
                                             stroke="#BBBBBB" stroke-width="2" stroke-linecap="round"
                                             stroke-linejoin="round" />
-                                    </svg></a>
+                                    </svg></div>
                                 <a href="{{ route('backend-management-faq-delete', $faq->id) }}"><svg width="24"
                                         height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M3 6H21" stroke="#BBBBBB" stroke-width="2" stroke-linecap="round"
@@ -646,16 +728,53 @@ h1.d-flex.text-dark.fw-bold.my-0.fs-1 {
                     </tbody>
                 </table>
             </div>
-        </div>
-        <div class="card-body py-4 table-footer">
-            <div class="pages">
-                Showing <span class="footer-page">1 to 10 of <span class="footer-page">96</span> entries
+            <div class="modal left fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+
+                        <div class="modal-body p-13">
+                            <div class="mb-4">
+                                <h4 class="mb-2 uname">
+                                    <- Edit FAQs</h4>
+                                        <h5 class="text-muted">#2121</h5>
+                            </div>
+                            <div>
+                                <div class="form-group">
+                                    <label>Question</label>
+                                    <textarea class="form-control" rows="3"></textarea>
+                                </div>
+                                <div class="form-group mt-10">
+                                    <label>Category</label>
+                                    <select class="form-control"></select>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="form-group">
+                                    <label>Answer</label>
+                                    <textarea class="form-control" rows="3"></textarea>
+                                </div>
+                                <div class="form-group mt-10">
+                                    <label>Sequence</label>
+                                    <select class="form-control"></select>
+                                </div>
+                                <div class="d-flex">
+                                    <button type="button" class="btn btn-primary">SaveChange</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="page-no"><span class="page-number">
-                </span> <span class="page-number active"> 1 </span> <span class="page-number"> 2 </span> <span
-                    class="page-number"> 3 </span> <span class="page-number"> . </span> <span class="page-number"> .
-                </span> <span class="page-number"> . </span> <span class="page-number"> > </span> </div>
+            <div class="card-body py-4 table-footer">
+                <div class="pages">
+                    Showing <span class="footer-page">1 to 10 of <span class="footer-page">96</span> entries
+                </div>
+                <div class="page-no"><span class="page-number">
+                    </span> <span class="page-number active"> 1 </span> <span class="page-number"> 2 </span> <span
+                        class="page-number"> 3 </span> <span class="page-number"> . </span> <span class="page-number"> .
+                    </span> <span class="page-number"> . </span> <span class="page-number"> > </span> </div>
+            </div>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
